076b1309305e0ba932ae5b3e0252373c5d9ecabd
brody <49502879+meloknight@users.noreply.github.com>
2024-05-09 10:58:55 -0700

    Initialize project, add scss, start on visual layout.

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import "./app.scss";
+
+function App() {
+  function Mountains() {
+    return (
+      <>
+        {/* distant mountains */}
+        <div className={`mountain distant-mountain md1`}></div>
+        <div className={`mountain distant-mountain md2`}></div>
+        <div className={`mountain distant-mountain md3`}></div>
+        <div className={`mountain distant-mountain md4`}></div>
+        <div className={`mountain distant-mountain md5`}></div>
+        <div className={`mountain distant-mountain md6`}></div>
+        <div className={`mountain distant-mountain md7`}></div>
+        {/* middle mountains */}
+        <div className={`mountain middle-mountain mm1`}></div>
+        <div className={`mountain middle-mountain mm2`}></div>
+        <div className={`mountain middle-mountain mm3`}></div>
+        <div className={`mountain middle-mountain mm4`}></div>
+        <div className={`mountain middle-mountain mm5`}></div>
+        <div className={`mountain middle-mountain mm6`}></div>
+        {/* close mountains */}
+        <div className={`mountain close-mountain mc0`}></div>
+        <div className={`mountain close-mountain mc1`}></div>
+        <div className={`mountain close-mountain mc2`}></div>
+        <div className={`mountain close-mountain mc3`}></div>
+        <div className={`mountain close-mountain mc4`}></div>
+        <div className={`mountain close-mountain mc5`}></div>
+        <div className={`mountain close-mountain mc6`}></div>
+        <div className={`mountain close-mountain mc7`}></div>
+      </>
+    );
+  }
+
+  function Clouds() {
+    return (
+      <>
+        <div className="cloud-container">
+          <div className="cloud-part-1"></div>
+          <div className="cloud-part-2"></div>
+          <div className="cloud-part-3"></div>
+          <div className="cloud-part-4"></div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="top-container">
+        <div className="sun"></div>
+        <Clouds />
+        <Mountains />
+      </div>
+      <div className="next-container"></div>
+    </>
+  );
+}
+
+export default App;
