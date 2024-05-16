4132fe42b8772c91a6e219cba13d029bed285384
brody <49502879+meloknight@users.noreply.github.com>
2024-05-16 11:52:51 -0700

    Added the fish svgs and the seaweed svgs to the bottom section.

--- src/App.tsx
+++ src/App.tsx
@@ -167,20 +167,107 @@ function App() {
           }`}
         >
           <InfoContainer />
-          <div
-            className={`mountain distant-mountain lower-left ${
-              theme === "night" ? "night-distant-mountain" : ""
-            }`}
-          ></div>
-          <div
-            className={`mountain distant-mountain lower-right ${
-              theme === "night" ? "night-distant-mountain" : ""
-            }`}
-          ></div>
+          <img
+            className="fish-1"
+            src="src/assets/fish-face-left.svg"
+            alt="fish-facing-left"
+          />
+          <img
+            className="fish-2"
+            src="src/assets/fish-face-left.svg"
+            alt="fish-facing-left"
+          />
+          <img
+            className="fish-3"
+            src="src/assets/fish-face-left.svg"
+            alt="fish-facing-left"
+          />
+          <img
+            className="fish-4"
+            src="src/assets/fish-face-left.svg"
+            alt="fish-facing-left"
+          />
+          <img
+            className="fish-5"
+            src="src/assets/fish-face-left.svg"
+            alt="fish-facing-left"
+          />
+          <img
+            className="fish-6"
+            src="src/assets/fish-face-left.svg"
+            alt="fish-facing-left"
+          />
+          <img
+            className="fish-7"
+            src="src/assets/fish-face-left.svg"
+            alt="fish-facing-left"
+          />
+          <img
+            className="fish-8"
+            src="src/assets/fish-face-left.svg"
+            alt="fish-facing-left"
+          />
+          <img
+            className="fish-9"
+            src="src/assets/fish-face-left.svg"
+            alt="fish-facing-left"
+          />
+          <img
+            className="fish-10"
+            src="src/assets/fish-face-left.svg"
+            alt="fish-facing-left"
+          />
+          <img
+            className="fish-11"
+            src="src/assets/fish-face-left.svg"
+            alt="fish-facing-left"
+          />
+          <img
+            className="seaweed-1"
+            src="src/assets/seaweed.svg"
+            alt="seaweed"
+          />
+          <img
+            className="seaweed-2"
+            src="src/assets/seaweed.svg"
+            alt="seaweed"
+          />
+          <img
+            className="seaweed-3"
+            src="src/assets/seaweed.svg"
+            alt="seaweed"
+          />
+          <img
+            className="seaweed-4"
+            src="src/assets/seaweed.svg"
+            alt="seaweed"
+          />
+          <img
+            className="seaweed-5"
+            src="src/assets/seaweed.svg"
+            alt="seaweed"
+          />
+          <img
+            className="seaweed-6"
+            src="src/assets/seaweed.svg"
+            alt="seaweed"
+          />
+          <img
+            className="seaweed-7"
+            src="src/assets/seaweed.svg"
+            alt="seaweed"
+          />
+          <img
+            className="seaweed-8"
+            src="src/assets/seaweed.svg"
+            alt="seaweed"
+          />
+          <img
+            className="seaweed-9"
+            src="src/assets/seaweed.svg"
+            alt="seaweed"
+          />
         </div>
-        <footer
-          className={`${theme === "night" ? "night-footer" : ""}`}
-        ></footer>
       </themeContext.Provider>
     </>
   );
