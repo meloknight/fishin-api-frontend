387dc5504f9ee97e0e220b0c18c3e6e315684159
brody <49502879+meloknight@users.noreply.github.com>
2024-05-22 15:40:20 -0700

    attempt 5 to fix bird svg's on production build.

--- src/components/Boats.tsx
+++ src/components/Boats.tsx
@@ -7,52 +7,52 @@ export default function Boats() {
     <>
       <img
         className={`boats boat-1 ${theme === "night" ? "night-boats" : ""}`}
-        src="src/assets/boat.svg"
+        src="./assets/boat.svg"
         alt="boat"
       />
       <img
         className={`boats boat-2 ${theme === "night" ? "night-boats" : ""}`}
-        src="src/assets/boat.svg"
+        src="./assets/boat.svg"
         alt="boat"
       />
       <img
         className={`boats boat-3 ${theme === "night" ? "night-boats" : ""}`}
-        src="src/assets/boat.svg"
+        src="./assets/boat.svg"
         alt="boat"
       />
       <img
         className={`boats boat-4 ${theme === "night" ? "night-boats" : ""}`}
-        src="src/assets/boat.svg"
+        src="./assets/boat.svg"
         alt="boat"
       />
       <img
         className={`boats boat-5 ${theme === "night" ? "night-boats" : ""}`}
-        src="src/assets/boat.svg"
+        src="./assets/boat.svg"
         alt="boat"
       />
       <img
         className={`boats boat-6 ${theme === "night" ? "night-boats" : ""}`}
-        src="src/assets/boat.svg"
+        src="./assets/boat.svg"
         alt="boat"
       />
       <img
         className={`boats boat-7 ${theme === "night" ? "night-boats" : ""}`}
-        src="src/assets/boat.svg"
+        src="./assets/boat.svg"
         alt="boat"
       />
       <img
         className={`boats boat-8 ${theme === "night" ? "night-boats" : ""}`}
-        src="src/assets/boat.svg"
+        src="./assets/boat.svg"
         alt="boat"
       />
       <img
         className={`boats boat-9 ${theme === "night" ? "night-boats" : ""}`}
-        src="src/assets/boat.svg"
+        src="./assets/boat.svg"
         alt="boat"
       />
       <img
         className={`boats boat-10 ${theme === "night" ? "night-boats" : ""}`}
-        src="src/assets/boat.svg"
+        src="./assets/boat.svg"
         alt="boat"
       />
     </>
--- src/components/FishAndSeaweed.tsx
+++ src/components/FishAndSeaweed.tsx
@@ -3,68 +3,68 @@ export default function FishAndSeaweed() {
     <>
       <img
         className="fish-1"
-        src="src/assets/fish-face-left.svg"
+        src="./assets/fish-face-left.svg"
         alt="fish-facing-left"
       />
       <img
         className="fish-2"
-        src="src/assets/fish-face-left.svg"
+        src="./assets/fish-face-left.svg"
         alt="fish-facing-left"
       />
       <img
         className="fish-3"
-        src="src/assets/fish-face-left.svg"
+        src="./assets/fish-face-left.svg"
         alt="fish-facing-left"
       />
       <img
         className="fish-4"
-        src="src/assets/fish-face-left.svg"
+        src="./assets/fish-face-left.svg"
         alt="fish-facing-left"
       />
       <img
         className="fish-5"
-        src="src/assets/fish-face-left.svg"
+        src="./assets/fish-face-left.svg"
         alt="fish-facing-left"
       />
       <img
         className="fish-6"
-        src="src/assets/fish-face-left.svg"
+        src="./assets/fish-face-left.svg"
         alt="fish-facing-left"
       />
       <img
         className="fish-7"
-        src="src/assets/fish-face-left.svg"
+        src="./assets/fish-face-left.svg"
         alt="fish-facing-left"
       />
       <img
         className="fish-8"
-        src="src/assets/fish-face-left.svg"
+        src="./assets/fish-face-left.svg"
         alt="fish-facing-left"
       />
       <img
         className="fish-9"
-        src="src/assets/fish-face-left.svg"
+        src="./assets/fish-face-left.svg"
         alt="fish-facing-left"
       />
       <img
         className="fish-10"
-        src="src/assets/fish-face-left.svg"
+        src="./assets/fish-face-left.svg"
         alt="fish-facing-left"
       />
       <img
         className="fish-11"
-        src="src/assets/fish-face-left.svg"
+        src="./assets/fish-face-left.svg"
         alt="fish-facing-left"
       />
-      <img className="seaweed-1" src="src/assets/seaweed.svg" alt="seaweed" />
-      <img className="seaweed-2" src="src/assets/seaweed.svg" alt="seaweed" />
-      <img className="seaweed-3" src="src/assets/seaweed.svg" alt="seaweed" />
-      <img className="seaweed-4" src="src/assets/seaweed.svg" alt="seaweed" />
-      <img className="seaweed-5" src="src/assets/seaweed.svg" alt="seaweed" />
-      <img className="seaweed-6" src="src/assets/seaweed.svg" alt="seaweed" />
-      <img className="seaweed-7" src="src/assets/seaweed.svg" alt="seaweed" />
-      <img className="seaweed-8" src="src/assets/seaweed.svg" alt="seaweed" />
-      <img className="seaweed-9" src="src/assets/seaweed.svg" alt="seaweed" />
+      <img className="seaweed-1" src="./assets/seaweed.svg" alt="seaweed" />
+      <img className="seaweed-2" src="./assets/seaweed.svg" alt="seaweed" />
+      <img className="seaweed-3" src="./assets/seaweed.svg" alt="seaweed" />
+      <img className="seaweed-4" src="./assets/seaweed.svg" alt="seaweed" />
+      <img className="seaweed-5" src="./assets/seaweed.svg" alt="seaweed" />
+      <img className="seaweed-6" src="./assets/seaweed.svg" alt="seaweed" />
+      <img className="seaweed-7" src="./assets/seaweed.svg" alt="seaweed" />
+      <img className="seaweed-8" src="./assets/seaweed.svg" alt="seaweed" />
+      <img className="seaweed-9" src="./assets/seaweed.svg" alt="seaweed" />
     </>
   );
 }
